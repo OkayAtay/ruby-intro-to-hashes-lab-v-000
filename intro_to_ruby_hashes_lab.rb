bbf2282aa913ea318737662e1c77d510af822c82
Araba Taylor <araba.taylor20@gmail.com>
2017-10-09 23:28:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,11 @@ def monopoly_with_second_tier
 		railroads:
 		{
 			pieces: 4
+			
+		names: {
+				
 			}
-			names: {}
-		}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+		}#
 end
 
 def monopoly_with_third_tier
